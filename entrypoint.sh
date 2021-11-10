aa99e42573e66c7a5a866cdc067680621d3ae278
gvv90 <vvg90@outlook.com>
2021-11-10 00:05:16 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-/bin/su '/opt/app_protect/bin/bd_agent &' nginx
-/bin/su "/usr/share/ts/bin/bd-socket-plugin tmm_count 4 proc_cpuinfo_cpu_mhz 2000000
-#total_xml_memory 307200000 total_umu_max_size 3129344 sys_max_account_id 1024 no_static_config 2>&1 >
-#/var/log/app_protect/bd-socket-plugin.log &" nginx
+/opt/app_protect/bin/bd_agent &
 /usr/sbin/nginx -g 'daemon off;'
